b8602d6930c8661c36d514af37d0a09e4f1b8a88
Colin Rundel <rundel@gmail.com>
2022-10-27 14:53:49 -0400

    tidy

--- R/gplm.R
+++ R/gplm.R
@@ -271,3 +271,32 @@ sp_predict_gplm = function(
   rownames(p) = stringr::str_glue("y[{seq_len(nrow(p))}]")
   posterior::as_draws(t(p))
 }
+
+
+#' @exportS3Method
+plot.gplm_fit = function(x, combo = c("dens", "trace"), ...) {
+  print(bayesplot::mcmc_combo(x$mcmc, combo = combo, ...))
+}
+
+#' @exportS3Method
+print.gplm_fit = function(x, ...) {
+  nchains = posterior::nchains(x$mcmc)
+  ndraws = posterior::ndraws(x$mcmc)
+  nvar = posterior::nvariables(x$mcmc)
+
+  cat( stringr::str_glue(
+    "# A gplm model (spBayes spLM) with {nchains} chains, {nvar} variables, and {ndraws} iterations."
+  ), "\n" )
+  summary(x)
+}
+
+#' @exportS3Method
+summary.gplm_fit = function(object, ...) {
+  print(summary(object$mcmc))
+}
+
+#' @exportS3Method
+print.spLM = function(x, ...) {
+  print("spLM model object")
+  invisible(x)
+}
